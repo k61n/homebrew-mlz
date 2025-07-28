f2adb50aa773ed33e0810bf95472ce75886cff20
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2025-07-28 10:31:09 +0200

    pytango v10.0.3

--- Formula/pytango.rb
+++ Formula/pytango.rb
@@ -4,8 +4,8 @@ class Pytango < Formula
   desc "Python module for cppTango C++ libraries"
   homepage "https://www.tango-controls.org/"
   url "https://gitlab.com/tango-controls/pytango.git",
-      tag: "v10.0.2"
-  version "10.0.2"
+      tag: "v10.0.3"
+  version "10.0.3"
 
   depends_on "git" => :build
   depends_on "numpy"
@@ -14,53 +14,53 @@ class Pytango < Formula
 
 
   resource "cp39_arm64" do
-    url "https://files.pythonhosted.org/packages/5a/3f/2ae51015acb0ab71772248527504e53c75f033dc069c3ade5cd97f8ea2ca/pytango-10.0.2-cp39-cp39-macosx_12_0_arm64.whl"
-    sha256 "07b72c1d74346bd276a7c0a84e557806ee7d351e65246cda5b8128323111392f"
+    url "https://files.pythonhosted.org/packages/81/35/21e88c717fe730ccf6c7f910b27c6fc817d3ce3eacf70d18b6dc0619dfaf/pytango-10.0.3-cp39-cp39-macosx_12_0_arm64.whl"
+    sha256 "777967d6d4f10f662941a1dcde984f89066b1d43d263192decbcd3ea7987d312"
   end
 
   resource "cp310_arm64" do
-    url "https://files.pythonhosted.org/packages/39/38/042f229a58029ffc7ca216ab335aec25576ff00bef87f09f4c3505f248c7/pytango-10.0.2-cp310-cp310-macosx_12_0_arm64.whl"
-    sha256 "b266c717460bbb6b02cb97eaf4c2ce230538b118a33e30da37c27b31b1f5f52f"
+    url "https://files.pythonhosted.org/packages/39/74/e0592346d74429f9aa5c759a94b343c204d08ec8184e9c61f93bb3f36431/pytango-10.0.3-cp310-cp310-macosx_12_0_arm64.whl"
+    sha256 "14a091601cab6af84f5ee255dd7f114ad67e9bc724b367099d13d994c8036ae6"
   end
 
   resource "cp311_arm64" do
-    url "https://files.pythonhosted.org/packages/eb/d6/d74e3d26ddc0de639c0187334288d63de28ab4002cdfb38c62c885edc909/pytango-10.0.2-cp311-cp311-macosx_12_0_arm64.whl"
-    sha256 "4e9320807ad5975a48e68db1fef5fa932a3971a534d55b0c5df21ef85c8b7b31"
+    url "https://files.pythonhosted.org/packages/11/a2/cfc28130704b44bae89d0cdec0e5f20878b5ef8a8bd7a822256fd5be335f/pytango-10.0.3-cp311-cp311-macosx_12_0_arm64.whl"
+    sha256 "51464e31b63b9f80687f84d9ece6d79ca82e8f5c483ccabf07c973cba15fd9d5"
   end
 
   resource "cp312_arm64" do
-    url "https://files.pythonhosted.org/packages/2f/8d/f8b4db3aadc46a059e9b571f25be2818634abc84e41b1609ae614ab2abdb/pytango-10.0.2-cp312-cp312-macosx_12_0_arm64.whl"
-    sha256 "37677cf9068ec560e5ac2c92eba6e337d7fc077964531cf1784c04b98903733a"
+    url "https://files.pythonhosted.org/packages/ed/fc/fdae94bcea73fc3e2952b89bf56f66512dff99bf3b9700fc0f585b2c8f98/pytango-10.0.3-cp312-cp312-macosx_12_0_arm64.whl"
+    sha256 "b90db10d2fe44c81260f91bf7e61949e5a913ff9f5909017d27a90a2a595e181"
   end
 
   resource "cp313_arm64" do
-    url "https://files.pythonhosted.org/packages/35/95/02acab0ddb31d839ede6f43e9f1a1c8cac5261bf45df418f602fa3caf550/pytango-10.0.2-cp313-cp313-macosx_12_0_arm64.whl"
-    sha256 "49ae5a4450062a94e51ebf894bb44dc96c1f7b02e9bd54dcf6c9bd734a026e96"
+    url "https://files.pythonhosted.org/packages/a4/a0/290468cd6d4038745d1de2cc58911c2e8596a9aa8d66b352802f96925292/pytango-10.0.3-cp313-cp313-macosx_12_0_arm64.whl"
+    sha256 "9a3f648f7dd66eeac5786ff9e315afa100064ff5873145d5c79a41321b56a57a"
   end
 
   resource "cp39_x86_64" do
-    url "https://files.pythonhosted.org/packages/63/36/7fe21490552b2f8370d1e401a07c9eed92d357a5aaf5e2d393aafba1acf3/pytango-10.0.2-cp39-cp39-macosx_12_0_x86_64.whl"
-    sha256 "f7cf46b9190cc1f6f52fafe32dc4dcf9f54c9a9cfeb452de8b25bca9c752c116"
+    url "https://files.pythonhosted.org/packages/5c/3c/ac23dbd3049434f2a4dd92dc2f7c83a7bef2dd0acc8f758ac26c1d6776bc/pytango-10.0.3-cp39-cp39-macosx_12_0_x86_64.whl"
+    sha256 "515b6a59bdada204e020e8f91c682392e015a428fdfb2a32041c10300fbffc58"
   end
 
   resource "cp310_x86_64" do
-    url "https://files.pythonhosted.org/packages/6d/ec/ee0ceefb57191bd2f7c49765f12b763b4feea967ffd1eceb8175e3a09a7f/pytango-10.0.2-cp310-cp310-macosx_12_0_x86_64.whl"
-    sha256 "06ac20a53a619e836d74cb402cfb638d0cf499f2836cba61a0a02b4309f1f663"
+    url "https://files.pythonhosted.org/packages/a7/f6/b639cf469a64089b62d40b4c41c117b608e968a644d50952783cd53a0288/pytango-10.0.3-cp310-cp310-macosx_12_0_x86_64.whl"
+    sha256 "82608e6d90666a248b6913c9b03f5a8b3d65a5a1c87acb004e22ec2326ef5a28"
   end
 
   resource "cp311_x86_64" do
-    url "https://files.pythonhosted.org/packages/d1/88/ee762aaea8d744686b88cd2d0b54618f99f73109c563a38e35ece7c51697/pytango-10.0.2-cp311-cp311-macosx_12_0_x86_64.whl"
-    sha256 "803747e11a7a94431f302a58650066d80d900bb5640568928d8a0d454a36c702"
+    url "https://files.pythonhosted.org/packages/b8/de/6774df781401aab59602c4dfbdc8fd17ceabdc546f5ed2248e0156b739d9/pytango-10.0.3-cp311-cp311-macosx_12_0_x86_64.whl"
+    sha256 "44ca2954feed026d55808b763b6666c58763cda60c6d5ab84867f2381b6ce705"
   end
 
   resource "cp312_x86_64" do
-    url "https://files.pythonhosted.org/packages/b2/8c/12436aeaee75465d8ee077d5fcd8eb73d49a325066daf552295361f0a6e2/pytango-10.0.2-cp312-cp312-macosx_12_0_x86_64.whl"
-    sha256 "42e9a2bbb925531e5b5e119176af5e18baefeb200942744dce168ee42ed67795"
+    url "https://files.pythonhosted.org/packages/cf/16/1e0e4d77c58e2eb3ead15aacbb852fed2343cb9a4c92022ea6b8c0297acf/pytango-10.0.3-cp312-cp312-macosx_12_0_x86_64.whl"
+    sha256 "ff313775ea4eb07224926026d48caa5fb41e143a61c17da626513834876b1b95"
   end
 
   resource "cp313_x86_64" do
-    url "https://files.pythonhosted.org/packages/49/a8/f5a47ca9fef44ac0d2a8eda5487f6bd507a77ed0ad766c30527d760d0dbf/pytango-10.0.2-cp313-cp313-macosx_12_0_x86_64.whl"
-    sha256 "3ab89fefeed30637e641d45920d376847688efb0f95543f329b0020f64961a41"
+    url "https://files.pythonhosted.org/packages/2c/54/e3cf1229f8f24b9f5323a09968828a3919742dc3931769a1f0892fa20f2b/pytango-10.0.3-cp313-cp313-macosx_12_0_x86_64.whl"
+    sha256 "07be8db2b72fe339ab525cabb3f0c5c833bbe47f685ab440ec99c4116b9d19de"
   end
 
 
