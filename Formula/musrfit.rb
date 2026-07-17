893b78e1a13fbb1c43e6af6bde48e32fbeaa21ca
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2026-07-17 11:58:48 +0200

    musrfit v1.11.3

--- Formula/musrfit.rb
+++ Formula/musrfit.rb
@@ -4,8 +4,8 @@ class Musrfit < Formula
   desc "muSR and beta-NMR data analysis package."
   homepage "https://lmu.web.psi.ch/musrfit/user/html/index.html"
   url "https://bitbucket.org/muonspin/musrfit.git",
-      tag: "v1.11.2"
-  version "1.11.2"
+      tag: "v1.11.3"
+  version "1.11.3"
 
   depends_on "cmake" => :build
   depends_on "git" => :build
