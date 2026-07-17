7f84ef0dfc6a5c1cc2c830a887ff5e4d59cf3ac3
Konstantin Kholostov <k.kholostov@fz-juelich.de>
2026-07-17 11:09:32 +0200

    cpptango v10.3.3

--- Formula/cpptango.rb
+++ Formula/cpptango.rb
@@ -4,8 +4,8 @@ class Cpptango < Formula
   desc "Distributed Control System - C++ library"
   homepage "https://www.tango-controls.org/"
   url "https://gitlab.com/tango-controls/cppTango.git",
-      tag: "10.3.2"
-  version "10.3.2"
+      tag: "10.3.3"
+  version "10.3.3"
 
   depends_on "cmake" => :build
   depends_on "git" => :build
